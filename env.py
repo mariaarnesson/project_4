6eeddcb0f824369f4b424b4a51bc0756df7e3507
mariaarnesson <maria_arnesson@outlook.com>
2023-01-26 12:35:24 +0000

    Test deploy

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://bomhfmms:tkoPtC_JQ0MSFGDW2T5pJJNdtbKtNhq2@snuffleupagus.db.elephantsql.com/bomhfmms"
+os.environ["SECRET_KEY"] = "my_big_surprise"
+os.environ["CLOUDINARY_URL"] = "cloudinary://227949597639582:ssX8xgBV9dzS3L5H8Nh8Vhr0kfg@dun5p7nsx"
